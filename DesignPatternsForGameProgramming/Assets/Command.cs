a6f1ae76057123805c1538d8bdc096634c791c0b
Iseta <isabelle.brandelero@gmail.com>
2020-09-30 15:19:34 -0300

    Alterações da aula 2

---
+++ DesignPatternsForGameProgramming/Assets/Command.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute(Animator anim);
+}
+
+public class PerformJump: Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isJumping");
+    }
+}
+
+public class DoNothing : Command
+{
+    public override void Execute(Animator anim)
+    {
+        
+    }
+}
+
+public class MoveForward : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isWalking");
+    }
+}
+
+public class PerformPunch : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isPunching");
+    }
+}
+
+public class PerformKick : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isKicking");
+    }
+}
---
+++ DesignPatternsForGameProgramming/Assets/InputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator anim;
+    Command keyQ, keyW, keyE, upArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyQ = new PerformJump();
+        keyW = new PerformPunch();
+        keyE = new PerformKick();
+        upArrow = new MoveForward();
+        anim = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            keyQ.Execute(anim);
+        }else if (Input.GetKeyDown(KeyCode.W))
+        {
+            keyW.Execute(anim);
+        }else if (Input.GetKeyDown(KeyCode.E))
+        {
+            keyE.Execute(anim);
+        }else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            upArrow.Execute(anim);
+        }
+    }
+}
